ca882fb3e03332889d0893475bea9c6481667602
phillip-h <phillip-h@users.noreply.github.com>
2020-11-24 16:49:43 -0700

    Add API example plugin

---
+++ api_example/src/main/java/acute/loot/apidemo/ALApiDemo.java
@@ -0,0 +1,55 @@
+package acute.loot.apidemo;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Plugin class. NO references to AcuteLoot here!
+ */
+public class ALApiDemo extends JavaPlugin {
+
+    private ALIntegration alIntegration;
+
+    @Override
+    public void onEnable() {
+        getLogger().info("Enabling ALApiDemo");
+        if (Bukkit.getServer().getPluginManager().getPlugin("AcuteLoot") != null) {
+            getLogger().info("AcuteLoot is enabled! Using integration.");
+            alIntegration = new ALIntegration(this);
+        } else {
+            getLogger().info("AcuteLoot is not enabled! Not using integration.");
+        }
+
+        checkAcuteLootApiVersion();
+        addAcuteLootEffectsIfAvailable();
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("Disabling ALApiDemo");
+        removeAcuteLootEffectsIfAvailable();
+        alIntegration = null;
+    }
+
+    private void checkAcuteLootApiVersion() {
+        if (alIntegration != null) {
+            if (alIntegration.checkVersion()) {
+                getLogger().info("AcuteLoot API matches requirements");
+            } else {
+                getLogger().warning("AcuteLoot API does not match requirements, features may not work!");
+            }
+        }
+    }
+
+    private void addAcuteLootEffectsIfAvailable() {
+        if (alIntegration != null) {
+            alIntegration.addEffects();
+        }
+    }
+
+    private void removeAcuteLootEffectsIfAvailable() {
+        if (alIntegration != null) {
+            alIntegration.removeEffects();
+        }
+    }
+}
