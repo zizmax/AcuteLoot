8ac6b5e40f628fc9b6530a9a2fb1eefa65f75849
zizmax <4u2njoy.nathan@gmail.com>
2021-01-26 18:40:09 -0700

    Basic loot well

--- src/main/java/acute/loot/Events.java
+++ src/main/java/acute/loot/Events.java
@@ -331,6 +331,31 @@ public class Events implements Listener {
                 effects.forEach(e -> e.apply(event));
             }
         }
+        if (plugin.getConfig().getBoolean("well.enabled") && player.hasPermission("acuteloot.use-well")){
+            new BukkitRunnable() {
+                @Override
+                public void run() {
+                    if(!event.getItemDrop().isValid() || event.getItemDrop() == null){
+                        cancel();
+                        return;
+                    }
+                    if(event.getItemDrop().isOnGround() || event.getItemDrop().isInWater()){
+                        Location well = new Location(plugin.getServer().getWorld(plugin.getConfig().getString("well.world")), plugin.getConfig().getDouble("well.x"), plugin.getConfig().getDouble("well.y"), plugin.getConfig().getDouble("well.z"));
+                        if(event.getItemDrop().getWorld().equals(well.getWorld())){
+                            if(event.getItemDrop().getLocation().distance(well) < plugin.getConfig().getDouble("well.max-distance")){
+                                event.getItemDrop().setItemStack(new ItemStack(Material.DIAMOND, event.getItemDrop().getItemStack().getAmount()));
+                                event.getItemDrop().setVelocity((player.getEyeLocation().add(0,10,0).toVector().subtract(event.getItemDrop().getLocation().toVector())).normalize().multiply(Double.parseDouble(player.getInventory().getItemInMainHand().getItemMeta().getDisplayName())));
+                                player.getWorld().spawnParticle(Particle.FIREWORKS_SPARK, well.add(0,.25, 0), 100);
+                                player.getWorld().playSound(well, Sound.ENTITY_GENERIC_EXPLODE, 1f, 2.5f);
+                            }
+                        }
+                    }
+
+                }
+            }.runTaskTimer(plugin, 20, 10);
+        }
+
+
         /*
         // May be used in the future to prevent Dead Eye from activating while viewing inventory
         // This "bug" could potentially be fixed by putting the entire Dead Eye logic into a Runnable that
