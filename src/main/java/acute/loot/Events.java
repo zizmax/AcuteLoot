b08d6308a712d3ed6d0d3b580e2698c66ef37910
Phillip Heikoop <phillip-h@users.noreply.github.com>
2021-02-20 08:27:55 -0700

    Abstract "pass through" events

--- src/main/java/acute/loot/Events.java
+++ src/main/java/acute/loot/Events.java
@@ -6,6 +6,7 @@ import org.bukkit.block.Block;
 import org.bukkit.block.Chest;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.*;
+import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -320,30 +321,6 @@ public class Events implements Listener {
         }
     }
 
-    @EventHandler()
-    public void onDropItem(PlayerDropItemEvent event){
-        Player player = event.getPlayer();
-        if (plugin.getConfig().getBoolean("effects.enabled") && player.getInventory().getChestplate() != null) {
-            String lootCode = getLootCode(plugin, player.getInventory().getChestplate());
-            if (lootCode != null) {
-                LootItem loot = new LootItem(lootCode);
-                List<LootSpecialEffect> effects = loot.getEffects();
-                effects.forEach(e -> e.apply(event));
-            }
-        }
-        /*
-        // May be used in the future to prevent Dead Eye from activating while viewing inventory
-        // This "bug" could potentially be fixed by putting the entire Dead Eye logic into a Runnable that
-        // executes in the next tick instead of the current tick. This would allow this event to mark that
-        // PlayerInteractEvent was actually associated with dropping an item out of inventory and NOT a true
-        // interaction. This event is called after PlayerInteractEvent so without delaying it by a tick,
-        // setting the association won't do anything.
-
-        // Example of what this event could do to mark its association with PlayerInteractEvent
-        deadEyeArrowsShot.putIfAbsent((Player) event.getPlayer(), -3);
-         */
-    }
-
     @EventHandler(priority = EventPriority.HIGH)
     // Priority HIGH to avoid conflicts with other plugins that modify fishing
     public void onPlayerFish(PlayerFishEvent event) {
@@ -378,20 +355,6 @@ public class Events implements Listener {
         }
     }
 
-    //XP Boost Effect
-    @EventHandler
-    public void onExpGain(PlayerExpChangeEvent event) {
-        Player player = event.getPlayer();
-        if (plugin.getConfig().getBoolean("effects.enabled") && player.getInventory().getHelmet() != null) {
-            String lootCode = getLootCode(plugin, player.getInventory().getHelmet());
-            if (lootCode != null) {
-                LootItem loot = new LootItem(lootCode);
-                List<LootSpecialEffect> effects = loot.getEffects();
-                effects.forEach(e -> e.apply(event));
-            }
-        }
-    }
-
     public int getEnchantRarity(Map<Enchantment, Integer> enchantments) {
         double total_levels = 0;
         for (Map.Entry<Enchantment, Integer> entry : enchantments.entrySet()) {
@@ -401,8 +364,8 @@ public class Events implements Listener {
 
     }
 
-
     public static String getLootCode(AcuteLoot plugin, ItemStack item) {
+        if (item == null) return null;
         ItemMeta meta = item.getItemMeta();
         if (meta == null) return null;
         NamespacedKey key = new NamespacedKey(plugin, "lootCodeKey");
@@ -431,13 +394,14 @@ public class Events implements Listener {
     }
 
     // Create Loot Item with RANDOM material
+
     public ItemStack createLootItem() {
         ItemStack item = chooseLootMaterial();
         return createLootItem(item, AcuteLoot.random.nextDouble());
     }
 
-
     // Create Loot Item with GIVEN material
+
     public ItemStack createLootItem(ItemStack item, double rarity) {
         // Generate loot: name, rarity and effects
         LootItemGenerator generator = new LootItemGenerator(AcuteLoot.rarityChancePool, AcuteLoot.effectChancePool);
@@ -451,7 +415,6 @@ public class Events implements Listener {
     //public ItemStack createLootItem(ItemStack item, int rarityID, List<Integer> effects) {
     //    return createLootItem(item, new LootItem(rarityID, effects));
     //}
-
     public ItemStack createLootItem(ItemStack item, LootRarity rarity) {
         LootItemGenerator generator = new LootItemGenerator(AcuteLoot.rarityChancePool, AcuteLoot.effectChancePool);
         final LootMaterial lootMaterial = LootMaterial.lootMaterialForMaterial(item.getType());
@@ -585,83 +548,80 @@ public class Events implements Listener {
         }
     }
 
+    //XP Boost Effect @EventHandler
+    public void onExpGain(PlayerExpChangeEvent event) {
+        applyPlayerEvent(event);
+    }
+
+
+    @EventHandler
+    public void onDropItem(PlayerDropItemEvent event){
+        applyPlayerEvent(event);
+        /*
+        // May be used in the future to prevent Dead Eye from activating while viewing inventory
+        // This "bug" could potentially be fixed by putting the entire Dead Eye logic into a Runnable that
+        // executes in the next tick instead of the current tick. This would allow this event to mark that
+        // PlayerInteractEvent was actually associated with dropping an item out of inventory and NOT a true
+        // interaction. This event is called after PlayerInteractEvent so without delaying it by a tick,
+        // setting the association won't do anything.
+
+        // Example of what this event could do to mark its association with PlayerInteractEvent
+        deadEyeArrowsShot.putIfAbsent((Player) event.getPlayer(), -3);
+         */
+    }
+
     @EventHandler
     public void onPlayerPickupItem(EntityPickupItemEvent event) {
         if (event.getEntity() instanceof Player) {
-            Player player = (Player) event.getEntity();
-            if (plugin.getConfig().getBoolean("effects.enabled") && player.getInventory().getChestplate() != null) {
-                String lootCode = getLootCode(plugin, player.getInventory().getChestplate());
-                if (lootCode != null) {
-                    LootItem loot = new LootItem(lootCode);
-                    List<LootSpecialEffect> effects = loot.getEffects();
-                    effects.forEach(e -> e.apply(event));
-                }
-            }
+            applyEventWithHumanEntity(event, (Player) event.getEntity());
         }
     }
 
     @EventHandler
     public void onPlayerStatisticIncrement(PlayerStatisticIncrementEvent event){
-        Player player = event.getPlayer();
-        if (plugin.getConfig().getBoolean("effects.enabled") && player.getInventory().getBoots() != null) {
-            String lootCode = getLootCode(plugin, player.getInventory().getBoots());
-            if (lootCode != null) {
-                LootItem loot = new LootItem(lootCode);
-                List<LootSpecialEffect> effects = loot.getEffects();
-                effects.forEach(e -> e.apply(event));
-            }
-        }
+        applyPlayerEvent(event);
     }
 
     @EventHandler
     public void onPlayerEnterBed(PlayerBedEnterEvent event) {
-        Player player = event.getPlayer();
-        if (plugin.getConfig().getBoolean("effects.enabled") && player.getInventory().getChestplate() != null) {
-            String lootCode = getLootCode(plugin, player.getInventory().getChestplate());
-            if (lootCode != null) {
-                LootItem loot = new LootItem(lootCode);
-                List<LootSpecialEffect> effects = loot.getEffects();
-                effects.forEach(e -> e.apply(event));
-            }
-        }
+        applyPlayerEvent(event);
     }
 
     @EventHandler
     public void onPlayerEnterPortal(PlayerPortalEvent event) {
-        Player player = event.getPlayer();
-        if (plugin.getConfig().getBoolean("effects.enabled") && player.getInventory().getChestplate() != null) {
-            String lootCode = getLootCode(plugin, player.getInventory().getChestplate());
-            if (lootCode != null) {
-                LootItem loot = new LootItem(lootCode);
-                List<LootSpecialEffect> effects = loot.getEffects();
-                effects.forEach(e -> e.apply(event));
-            }
-        }
+        applyPlayerEvent(event);
     }
 
     @EventHandler
     public void onPlayerHoldItem(PlayerItemHeldEvent event) {
-        Player player = event.getPlayer();
-        if (plugin.getConfig().getBoolean("effects.enabled") && player.getInventory().getChestplate() != null) {
-            String lootCode = getLootCode(plugin, player.getInventory().getChestplate());
-            if (lootCode != null) {
-                LootItem loot = new LootItem(lootCode);
-                List<LootSpecialEffect> effects = loot.getEffects();
-                effects.forEach(e -> e.apply(event));
-            }
-        }
+        applyPlayerEvent(event);
     }
 
     @EventHandler
     public void onPlayerClickTonight(InventoryClickEvent event) {
-        Player player = (Player) event.getWhoClicked();
-        if (plugin.getConfig().getBoolean("effects.enabled") && player.getInventory().getChestplate() != null) {
-            String lootCode = getLootCode(plugin, player.getInventory().getChestplate());
-            if (lootCode != null) {
-                LootItem loot = new LootItem(lootCode);
-                List<LootSpecialEffect> effects = loot.getEffects();
-                effects.forEach(e -> e.apply(event));
-            }
+        applyEventWithHumanEntity(event, event.getWhoClicked());
+    }
+
+    private void applyPlayerEvent(final PlayerEvent event) {
+        applyEventWithHumanEntity(event, event.getPlayer());
+    }
+
+    private void applyEventWithHumanEntity(final Event event, final HumanEntity player) {
+        applyEventToItem(event, player.getInventory().getItemInMainHand());
+        applyEventToItem(event, player.getInventory().getItemInOffHand());
+        applyEventToItem(event, player.getInventory().getHelmet());
+        applyEventToItem(event, player.getInventory().getChestplate());
+        applyEventToItem(event, player.getInventory().getLeggings());
+        applyEventToItem(event, player.getInventory().getBoots());
+    }
+
+    private void applyEventToItem(final Event event, final ItemStack item) {
+        if (!plugin.getConfig().getBoolean("effects.enabled")) {
+            return;
+        }
+        String lootCode = getLootCode(plugin, item);
+        if (lootCode != null) {
+            new LootItem(lootCode).getEffects().forEach(e -> e.apply(event));
         }
     }
 
